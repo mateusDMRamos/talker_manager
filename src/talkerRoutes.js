ea90b0bf57a82060db920fa36bc9a6705461e489
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-02-06 15:41:26 -0300

    feat: criado método get para retornar apenas o id de um palestrante

--- src/talkerRoutes.js
+++ src/talkerRoutes.js
@@ -5,12 +5,29 @@ const { join } = require('path');
 const router = express.Router();
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND = 404;
 
-router.get('/', async (_req, res) => {
+const getTakers = async () => {
   const path = './talker.json';
   const data = await fs.readFile(join(__dirname, path), 'utf-8');
-  const talkers = JSON.parse(data);
+  return JSON.parse(data);
+};
+
+router.get('/', async (_req, res) => {
+  const talkers = await getTakers();
   res.status(HTTP_OK_STATUS).json([...talkers]);
+});
+
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const talkers = await getTakers();
+  const selectedTalker = talkers.find((talker) => talker.id === id);
+  if (!selectedTalker) {
+    return res.status(HTTP_NOT_FOUND).json({
+      message: 'Pessoa palestrante não encontrada',
+    });
+  } 
+  res.status(HTTP_OK_STATUS).json(selectedTalker);
 });
 
 module.exports = router;

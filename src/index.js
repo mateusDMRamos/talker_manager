ecec32c9270731a30b833466df0e5e9cd6d1e2c3
Mateus de Medeiros Ramos <mateusdmramos@gmail.com>
2023-02-06 15:07:06 -0300

    feat: criada requisição get para o path /talker

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const { join } = require('path');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const path = './talker.json';
+  const talkers = await fs.readFile(path.resolve(join(__dirname, path), './talker.json'));
+  res.status(HTTP_OK_STATUS).json({ talkers });
 });
 
 app.listen(PORT, () => {
